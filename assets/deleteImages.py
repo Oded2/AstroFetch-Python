7f82951d9f8435ff1ae8840e63c5c1055849fe99
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-06-29 00:46:51 +0300

    Initial commit

---
+++ assets/deleteImages.py
@@ -0,0 +1,12 @@
+import os
+
+
+def delete_files_in_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+
+folder_path = "images"
+delete_files_in_folder(folder_path)
--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import requests
+import json
+import os
+
+
+def get_file_extension(file_path):
+    _, extension = os.path.splitext(file_path)
+    return extension
+
+
+def check_file_exists(directory, filename):
+    file_path = os.path.join(directory, filename)
+    return os.path.isfile(file_path)
 
 
 def enterToContinue():
-    input("Press ENTER to ")
+    input("Press ENTER to continue")
 
 
 url = "https://api.nasa.gov/planetary/apod"
-
-
 api_key = "RH5e6AJeKi4RfZo11EoRbhCwlGspTA2Cdc6nZgoG"
-# api_key = "RH5e6AJeKi4RfZo11EoRbhCwlGspTA2Cdc6nZ"
 
-# day = input("Enter Day ")
-# month = input("Enter Month ")
-# year = input("Enter Year ")
+with open("params.json", 'r') as f:
+    params = json.load(f)
 
-# date = f"{year}-{month}-{day}"
-# print(date)
-params = {
-    "date": "2022-8-1",
-    "api_key": api_key
-
-}
-
-
-response = requests.get(url, params=params)
-print(response.url)
+response = requests.get(url, params=params, timeout=30)
 
 if not response.status_code == 200:
     if response.status_code == 403:
@@ -35,14 +33,23 @@ if not response.status_code == 200:
 
     exit()
 data = response.json()
+with open("test/test.json", 'w') as f:
+    json.dump(data, f, indent=6)
+for i in range(len(data)):
 
-# Extract the required information
-title = data["title"]
-explanation = data["explanation"]
-image_url = data["url"]
+    title = data[i]["title"]
 
-# Do something with the data
-print("Title:", title)
-new_response = requests.get(image_url)
-with open(f"images/{title}.jpg", 'wb') as f:
-    f.write(new_response.content)
+    image_url = data[i]["url"]
+    image_extension = get_file_extension(image_url)
+    print(f"Processing image: {title}")
+    new_response = requests.get(image_url)
+    file_name = f"{title}{image_extension}"
+    print(file_name)
+    if not check_file_exists("images", f"{file_name}"):
+
+        with open(f"images/{file_name}", 'wb') as f:
+            f.write(new_response.content)
+    else:
+        print(title + " already exists")
+
+print("Operation Finished")
